b8d651a96eeab89beb01bdf8241928f9998b9676
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2018-05-28 06:14:51 +0400

    update

--- src/pug/inc/javascript.js
+++ src/pug/inc/javascript.js
@@ -1,6 +1,6 @@
 var time = (new Date()).getTime();
-yepnope('/assets/templates/comingsoon/js/appjs.js?'+time, undefined, function() {
-	yepnope('/assets/templates/comingsoon/js/main.js?'+time, undefined, function() {
+yepnope('https://art-garage.github.io/comingsoon/assets/templates/comingsoon/js/appjs.js?'+time, undefined, function() {
+	yepnope('https://art-garage.github.io/comingsoon/assets/templates/comingsoon/js/main.js?'+time, undefined, function() {
 	
 	})
 })
