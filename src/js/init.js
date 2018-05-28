47043c0acecf25fd0e7b1df27c2f848097f2dfca
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2018-05-28 06:40:51 +0400

    update

--- src/js/init.js
+++ src/js/init.js
@@ -1,8 +1,11 @@
 /*content enabled*/
-setTimeout(function(){
-	$('body .preloader').animate({
-		opacity: 0
-	}, 1500, function(){
-		$('body .preloader').hide().parent().removeClass('load');
-	});
-}, 4000);
+$(window).on('load', function(){
+	var preloader = $('body .preloader');
+	setTimeout(function(){
+		preloader.animate({
+			opacity: 0
+		}, 1500, function(){
+			preloader.hide().parent().removeClass('load');
+		});
+	}, 4000);
+});
